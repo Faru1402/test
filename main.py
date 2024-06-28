f17d4b8ea3257591dff066cb3b90118fb8b79d60
felipedcp20 <felipedcp@gmail.com>
2024-06-28 16:25:47 -0500

    Modify profile heroku

--- main.py
+++ main.py
@@ -7,9 +7,53 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"backend": "0.01"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/get_product_specification/")
+def get_product_specification():
+    """function to get product specification"""
+    info = {
+            "MANICURE": {
+            "Acrilicas": "$150,000",
+            "Polygel": "$125,000",
+            "Press On": "$110,000",
+            "Semipermanente": "$40,000",
+            "Tradicional": "$20,000"
+            },
+            "PEDICURE": {
+            "Semipermanente": "$50,000",
+            "Tradicional": "$30,000"
+            }
+            }
+
+    
+    return{"result": info}
+
+
+@app.get("/get_service_price/{category}/{subcategory}")
+def get_service_price(category: str, subcategory: str):
+    """function to get service price"""
+
+    def get_service_price(self,category, subcategory):
+        info = {
+        "MANICURE": {
+        "Acrilicas": "$150,000",
+        "Polygel": "$125,000",
+        "Press On": "$110,000",
+        "Semipermanente": "$40,000",
+        "Tradicional": "$20,000"
+        },
+        "PEDICURE": {
+        "Semipermanente": "$50,000",
+        "Tradicional": "$30,000"
+        }
+        }
+
+        if category in info and subcategory in info[category]:
+            return info[category][subcategory]
+        else:
+            return "Combination not found"
+        
+    result = get_service_price(category, subcategory)
+    return {"result": result}

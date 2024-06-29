4c8aa02e60a962807ccf7c508de2bad91c74fb27
felipedcp20 <felipedcp@gmail.com>
2024-06-29 07:56:14 -0500

    Add raise exception

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -35,25 +34,23 @@ def get_product_specification():
 def get_service_price(category: str, subcategory: str):
     """function to get service price"""
 
-    def get_service_price(self,category, subcategory):
-        info = {
-        "MANICURE": {
-        "Acrilicas": "$150,000",
-        "Polygel": "$125,000",
-        "Press On": "$110,000",
-        "Semipermanente": "$40,000",
-        "Tradicional": "$20,000"
-        },
-        "PEDICURE": {
-        "Semipermanente": "$50,000",
-        "Tradicional": "$30,000"
-        }
-        }
+    info = {
+    "MANICURE": {
+    "Acrilicas": "$150,000",
+    "Polygel": "$125,000",
+    "Press On": "$110,000",
+    "Semipermanente": "$40,000",
+    "Tradicional": "$20,000"
+    },
+    "PEDICURE": {
+    "Semipermanente": "$50,000",
+    "Tradicional": "$30,000"
+    }
+    }
 
-        if category in info and subcategory in info[category]:
-            return info[category][subcategory]
-        else:
-            return "Combination not found"
+    if category in info and subcategory in info[category]:
+        result = info[category][subcategory]
+    else:
+        raise HTTPException(status_code=404, detail="Item not found")
         
-    result = get_service_price(category, subcategory)
     return {"result": result}
